479631f6b807799fdee9f85e997e36fa7be45a32
Michal Dubiel <md@semihalf.com>
2015-01-15 11:47:34 -0500

    build.sh: Automatically apply Contrail patches from patches/<pkg_name>/

--- build.sh
+++ build.sh
@@ -9,10 +9,31 @@ for pkg in $pkg_dsc_list; do
     dpkg-source -x $pkg
 done
 
+# Apply patches
+for dir in $src_dir_list; do
+    cd $dir
+
+    dir_name=$(echo "$dir" | sed -re 's/(\S+)-\S*/\1/')
+    patch_dir=$(find ../patches -type d -name $dir_name)
+
+    # Skip if no patches dir
+    if [ -z "$patch_dir" ]; then
+        cd ../
+        continue
+    fi
+
+    for p in $(ls $patch_dir); do
+        echo "Patching $dir_name with $p"
+        patch -N -p1 < "$patch_dir/$p"
+    done
+
+    cd ../
+done
+
 # Build
 for dir in $src_dir_list; do
     cd $dir
-    dpkg-buildpackage -uc -b
+    dpkg-buildpackage -uc -b -j5
     cd ../
 done
 
