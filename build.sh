c52ec4ed686869ff829a3237dbbb586e8561773f
Michal Dubiel <md@semihalf.com>
2015-08-01 16:23:50 +0200

    build: New version using pbuilder

--- build.sh
+++ build.sh
@@ -1,42 +1,131 @@
 #!/usr/bin/env bash
 
-pkg_dsc_list=$(ls *.dsc)
-src_dir_list=$(ls *.orig*z | sed -re 's/^(\S+)\.orig\S+/\1/' | sed -re 's/_/-/')
+PBUILDER_CONF="./config/pbuilderrc"
+PKGS_DIR="."
+PATCHES_DIR="patches"
+OUT_DIR="debs"
+
+export SCRIPT_DIR=$(readlink -f $(dirname $0i))
+
+usage() {
+    if [ -n "$1" ]; then
+        echo "${0##*/} error: $1"
+    else
+        echo "Build deb packages in clean chroot"
+        echo "Usage:"
+        echo "    ${0##*/} <dist> [<pkg>]..."
+        echo "    <dist> - distro codename (trusty, precise)"
+        echo "    <pkg>  - pkg to build, all if empty"
+        echo "Example:"
+        echo "    ${0##*/} trusty libvirt"
+    fi
+    exit 1
+}
+
+get_src_dir() {
+    name="$1"
+
+    src_dir=$(ls "${PKGS_DIR}/${name}_"*.orig*z)
+    src_dir=${src_dir%%.orig*}
+    src_dir=${src_dir/_/-}
+
+    echo "$src_dir"
+}
+
+run() {
+    "$@"
+    if [ $? -ne 0 ]; then
+        echo "Error executing: $@"
+        exit 1
+    fi
+}
+
+run_in_dir() {
+    dir=$1
+    shift
+
+    pushd $dir
+    "$@"
+    if [ $? -ne 0 ]; then
+        echo "Error executing: $@"
+        exit 1
+    fi
+    popd
+}
+
+# Verify cmd arguments
+if [ -z "$1" ]; then
+    usage
+fi
+
+# Clean
+if [ "$1" == "clean" ]; then
+    run rm -rf $OUT_DIR
+    exit 0
+fi
+
+# Get distro and put to DIST env used by pbuilder
+export DIST=$1
+shift
+
+# Create base system if necessary
+. $PBUILDER_CONF
+if [ ! -s "$BASETGZ" ]; then
+    echo "Creating base system..."
+    run pbuilder --create --configfile $PBUILDER_CONF
+fi
+
+# Get pkgs to build
+pkgs=$@
+if [ -z "$pkgs" ]; then
+    for dsc in $(ls "$PKGS_DIR"/*.dsc); do
+        dsc=${dsc##*/}
+        pkgs+="${dsc%%_*} "
+    done
+fi
 
 # Extract sources
-rm -rf $src_dir_list
-for pkg in $pkg_dsc_list; do
-    dpkg-source -x $pkg
+for name in $pkgs; do
+    dsc=$(ls "${PKGS_DIR}/${name}_"*.dsc)
+    src_dir=$(get_src_dir $name)
+    run rm -rf $src_dir
+    run dpkg-source -x $dsc
 done
 
 # Apply patches
-for dir in $src_dir_list; do
-    cd $dir
+for name in $pkgs; do
+    src_dir=$(get_src_dir $name)
 
-    dir_name=$(echo "$dir" | sed -re 's/(\S+)-\S*/\1/')
-    patch_dir=$(find ../patches -type d -name $dir_name)
-
-    # Skip if no patches dir
-    if [ -z "$patch_dir" ]; then
-        cd ../
-        continue
-    fi
-
-    for p in $(ls $patch_dir); do
-        echo "Patching $dir_name with $p"
-        patch -N -p1 < "$patch_dir/$p"
+    # Apply the DEBIAN-* pathes
+    patches=$(ls "${PATCHES_DIR}/${DIST}/${name}/DEBIAN-"*.patch)
+    for patch in $patches; do
+        patch=$(readlink -e $patch)
+        run_in_dir $src_dir patch -N -p1 < $patch
+        #run "(cd $src_dir && patch -N -p1 < $patch)"
     done
 
-    cd ../
+    # Apply the CODE-* patches
+    patches=$(ls "${PATCHES_DIR}/${DIST}/${name}/CODE-"*.patch)
+    for patch in $patches; do
+        patch=$(readlink -e $patch)
+        run_in_dir $src_dir quilt import $patch && quilt push
+        #(cd $src_dir; quilt import $patch && quilt push)
+    done
 done
 
-# Build
-for dir in $src_dir_list; do
-    cd $dir
-    dpkg-buildpackage -uc -b -j5 || exit 1
-    cd ../
+# Build packages
+for name in $pkgs; do
+    src_dir=$(get_src_dir $name)
+
+    conf=$(readlink -e "$PBUILDER_CONF")
+    run mkdir -p "$BUILDRESULT"
+    run rm -f "$BUILDRESULT/${name}_"*.deb
+
+    run_in_dir $src_dir pdebuild --configfile $conf --use-pdebuild-internal
 done
 
 # Clean up
-rm -rf $src_dir_list
-rm -rf *.changelog
+for name in $pkgs; do
+    src_dir=$(get_src_dir $name)
+    run rm -f "${name}_"*.deb
+done
